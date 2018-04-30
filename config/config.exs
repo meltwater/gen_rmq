9f843b750b57507472b6c716e8b319f478a805a7
Mateusz Korszun <mkorszun@gmail.com>
2018-04-30 16:22:11 +0200

    Improve test coverage (#12)

--- config/config.exs
+++ config/config.exs
@@ -18,7 +18,7 @@ use Mix.Config
 #
 # You can also configure a 3rd-party app:
 #
-config :logger, level: :warn
+config :logger, level: :error
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
