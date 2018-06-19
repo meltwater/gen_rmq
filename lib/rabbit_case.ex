ab3fdaaaf577ec82eda5f0edcf4dd91073dc9072
mkorszun <mkorszun@gmail.com>
2018-06-19 16:24:37 +0200

    Extend purge_queue helper to ignore non-existing queues

--- lib/rabbit_case.ex
+++ lib/rabbit_case.ex
@@ -41,7 +41,22 @@ defmodule GenRMQ.RabbitCase do
         AMQP.Connection.close(conn)
       end
 
+      def purge_queues!(uri, queues) do
+        {:ok, conn} = rmq_open(uri)
+        Enum.each(queues, &purge_queue!(conn, &1))
+        AMQP.Connection.close(conn)
+      end
+
       def purge_queue(conn, queue) do
+        try do
+          purge_queue!(conn, queue)
+        catch
+          :exit, _ ->
+            :ok
+        end
+      end
+
+      def purge_queue!(conn, queue) do
         {:ok, chan} = AMQP.Channel.open(conn)
         AMQP.Queue.purge(chan, queue)
         AMQP.Channel.close(chan)
