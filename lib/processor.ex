317c475291c49b51ec12adf0a03ed3c4931041ae
Joel C <joel.carlbark@meltwater.com>
2020-03-09 08:45:11 +0100

    Add example and more detailed description for GenRMQ.Processor (#146)

--- lib/processor.ex
+++ lib/processor.ex
@@ -1,6 +1,10 @@
 defmodule GenRMQ.Processor do
   @moduledoc """
-  Defines functions to implement by any AMQP processor
+  Behaviour module for implementing a RabbitMQ message processor.
+
+  A message processor is typically used to separate out business logic from a consumer,
+  to let the consumer only deal with RabbitMQ specifics.
   """
+
   @callback process(message :: %GenRMQ.Message{}) :: :ok | {:error, term}
 end
